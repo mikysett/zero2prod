0dd679c125207b56fa3da6e7bfc63b3efe465196
Michele Sessa <mikysett@gmail.com>
2024-08-15 18:22:37 +0100

    use validator to parse email address

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,15 +1,53 @@
+use validator::validate_email;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     pub fn parse(s: String) -> Result<SubscriberEmail, String> {
         // TODO: add validation
-        Ok(Self(s))
+        if validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email", s))
+        }
     }
 }
 
 impl AsRef<str> for SubscriberEmail {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberEmail;
+    use claims::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberEmail::parse(name));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let name = "ursuladomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(name));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let name = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(name));
+    }
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+        claims::assert_ok!(SubscriberEmail::parse(email));
     }
 }
