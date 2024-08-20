9fb46bbecef894d63bd3dbc7aa6ad6395136ee1f
Michele Sessa <mikysett@gmail.com>
2024-08-20 23:31:00 +0100

    refactor integration tests and extract application build logic to avoid repetition and include it in tests

---
+++ tests/api/helpers.rs
@@ -0,0 +1,73 @@
+use once_cell::sync::Lazy;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::startup::get_connection_pool;
+use zero2prod::startup::Application;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(
+            "zero2prod".to_string(),
+            "info".to_string(),
+            std::io::stdout,
+        );
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(
+            "zero2prod".to_string(),
+            "info".to_string(),
+            std::io::sink,
+        );
+        init_subscriber(subscriber);
+    }
+});
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+pub async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
+    let mut configuration =
+        get_configuration().expect("Failed to read configuration");
+    configuration.application.port = 0;
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    configure_database(&configuration.database).await;
+
+    let application = Application::build(configuration.clone())
+        .await
+        .expect("Failed to build application");
+    let address = format!("http://127.0.0.1:{}", application.port());
+    let _ = tokio::spawn(application.run_until_stopped());
+
+    TestApp {
+        address,
+        db_pool: get_connection_pool(&configuration.database),
+    }
+}
+
+async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(
+            format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str(),
+        )
+        .await
+        .expect("Failed to create database.");
+
+    let connection_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
