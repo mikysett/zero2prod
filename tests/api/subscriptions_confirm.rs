e8bc675195b5f1b96f49e5b4cc23a6c52f6405c4
Michele Sessa <mikysett@gmail.com>
2024-08-31 00:37:41 +0100

    add integration test to check status is not changed to confirmed if not pending_confirmation

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -128,3 +128,41 @@ async fn confirm_returns_a_400_when_token_do_not_exist() {
 
     assert_eq!(response.status().as_u16(), 400);
 }
+
+#[tokio::test]
+async fn confirm_do_not_change_the_status_if_not_pending_confirmation() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    sqlx::query!(
+        r#"
+        UPDATE subscriptions
+        SET status = 'another status'
+        WHERE email = 'ursula_le_guin@gmail.com'
+        "#
+    )
+    .execute(&app.db_pool)
+    .await
+    .expect("Failed to change subscriber status");
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
+    assert_eq!(response.status().as_u16(), 200);
+
+    let saved = sqlx::query!("SELECT status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.status, "another status");
+}
