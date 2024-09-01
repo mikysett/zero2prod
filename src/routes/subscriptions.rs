3a6289483c89d42d04cc4d332c14c8417594178d
Michele Sessa <mikysett@gmail.com>
2024-09-01 17:17:02 +0100

    move error_chain_fmt in an helper file for more reusability

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,7 @@ use crate::domain::SubscriberEmail;
 use crate::domain::SubscriberName;
 use crate::domain::SubscriptionToken;
 use crate::email_client::EmailClient;
+use crate::routes::helpers::error_chain_fmt;
 use crate::startup::ApplicationBaseUrl;
 
 #[derive(serde::Deserialize)]
@@ -147,19 +148,6 @@ impl std::fmt::Debug for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
     }
-}
-
-fn error_chain_fmt(
-    e: &impl std::error::Error,
-    f: &mut std::fmt::Formatter<'_>,
-) -> std::fmt::Result {
-    writeln!(f, "{}\n", e)?;
-    let mut current = e.source();
-    while let Some(cause) = current {
-        writeln!(f, "Caused by:\n\t{}", cause)?;
-        current = cause.source()
-    }
-    Ok(())
 }
 
 impl std::fmt::Display for StoreTokenError {
