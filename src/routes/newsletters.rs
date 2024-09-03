132ad5f79dc441733279c83b6d76db883d3d824b
Michele Sessa <mikysett@gmail.com>
2024-09-03 01:02:10 +0100

    implement get confirmed subscribers

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,4 +1,8 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{http::StatusCode, web, HttpResponse};
+use anyhow::Context;
+use sqlx::PgPool;
+
+use crate::routes::helpers::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -12,6 +16,55 @@ pub struct Content {
     text: String,
 }
 
-pub async fn newsletter(_body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl actix_web::ResponseError for PublishError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => {
+                StatusCode::INTERNAL_SERVER_ERROR
+            }
+        }
+    }
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+pub async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, sqlx::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+            SELECT email
+            FROM subscriptions
+            WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+    Ok(rows)
+}
+
+pub async fn newsletter(
+    _body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool)
+        .await
+        .context("Failed to get confirmed subscribers")?;
+    Ok(HttpResponse::Ok().finish())
 }
