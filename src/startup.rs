92fe273308679b54c6d8fa6a74cdae80859b8653
Michele Sessa <mikysett@gmail.com>
2024-09-21 17:58:51 +0100

    add hmac to application data

--- src/startup.rs
+++ src/startup.rs
@@ -9,7 +9,9 @@ use crate::routes::login_form;
 use crate::routes::publish_newsletter;
 use crate::routes::subscribe;
 use actix_web::web;
+use actix_web::web::Data;
 use actix_web::{dev::Server, App, HttpServer};
+use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -19,6 +21,8 @@ pub struct Application {
     port: u16,
     server: Server,
 }
+
+pub struct HmacSecret(pub Secret<String>);
 
 impl Application {
     pub async fn build(
@@ -50,6 +54,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -77,6 +82,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
@@ -94,6 +100,7 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
