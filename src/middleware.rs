6b03436caf00b5d18930c83969e84456848a9c52
Michele Sessa <mikysett@gmail.com>
2024-09-29 00:21:20 +0100

    improve auth guard by redirect to login anonymous users

--- src/middleware.rs
+++ src/middleware.rs
@@ -3,24 +3,24 @@ use crate::{
     utils::{e500, see_other},
 };
 use actix_web::{
-    body::{BoxBody, EitherBody, MessageBody},
+    body::MessageBody,
     dev::{ServiceRequest, ServiceResponse},
+    error::InternalError,
     middleware::Next,
 };
 
 pub async fn auth_guard(
     mut req: ServiceRequest,
     next: Next<impl MessageBody>,
-) -> Result<
-    ServiceResponse<EitherBody<BoxBody, impl MessageBody>>,
-    actix_web::Error,
-> {
+) -> Result<ServiceResponse<impl MessageBody>, actix_web::Error> {
     // Extract the session from the request
     let session = req.extract::<TypedSession>().await.map_err(e500)?;
 
     // pre-processing - Check that a username exists for the given user_id in the session
     if session.get_user_id().map_err(e500)?.is_none() {
-        return Ok(req.into_response(see_other("/login").map_into_left_body()));
+        let response = see_other("/login");
+        let e = anyhow::anyhow!("The user has not logged in");
+        return Err(InternalError::from_response(e, response).into());
     }
 
     // invoke the wrapped middleware or service
@@ -28,5 +28,5 @@ pub async fn auth_guard(
 
     // post-processing
 
-    Ok(res.map_into_right_body())
+    Ok(res)
 }
